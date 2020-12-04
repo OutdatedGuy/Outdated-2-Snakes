6d4187f1219f7b125abcc27c63dc47f84d0747ec
OutdatedGuy <everythingoutdated@gmail.com>
2020-12-05 01:00:05 +0530

    Removing dotenv

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
 
 const port = process.env.PORT || 1412;
 app.listen(port, () => console.log(`Starting server at ${port}`));
